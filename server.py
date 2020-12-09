ffb8c7201ae397903c8cc4e7c6e1ac010fe34176
Nathan Russell <70409943+TheGamingArcher@users.noreply.github.com>
2020-12-08 22:45:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,24 @@
 import socket
 import threading
 
+
 class Server:
     def __init__(self):
         self.start_server()
 
     def start_server(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         host = socket.gethostbyname(socket.gethostname())
         port = int(input('Enter port to run the server on --> '))
 
         self.clients = []
 
-        self.s.bind((host,port))
+        self.s.bind((host, port))
         self.s.listen(100)
-    
-        print('Running on host: '+str(host))
-        print('Running on port: '+str(port))
+
+        print('Running on host: ' + str(host))
+        print('Running on port: ' + str(port))
 
         self.username_lookup = {}
 
@@ -25,21 +26,22 @@ class Server:
             c, addr = self.s.accept()
 
             username = c.recv(1024).decode()
-            
-            print('New connection. Username: '+str(username))
-            self.broadcast('New person joined the room. Username: '+username)
+
+            print('New connection. Username: ' + str(username))
+            self.broadcast('New person joined the room. Username: ' + username)
 
             self.username_lookup[c] = username
 
             self.clients.append(c)
-             
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
-    def broadcast(self,msg):
+            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
+
+    def broadcast(self, msg):
         for connection in self.clients:
             connection.send(msg.encode())
 
-
+#Handle client was modified to only show new connections and if the user left the chatroom
+#The password is not shown to the server, only checks against the users.
     def handle_client(self, c, addr):
         while True:
             try:
